47d8cdf2bd699be4a9ed205fa1155c2b71b2608b
Nick Adams <nick.pappas.adams@gmail.com>
2023-06-24 12:53:41 -0700

    adding frontend folder

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { FaSignInAlt } from 'react-icons/fa';
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+       
+        email: "",
+        password: "",
+        
+      });
+    
+      const { email, password } = formData;
+    
+      const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState, 
+            [e.target.name]: e.target.value
+        }))
+      }
+    
+      const onSubmit = (e) => {
+        e.preventDefault()
+      }
+    
+      return (
+        <>
+          <section classname="heading">
+            <h1>
+              <FaSignInAlt />
+              Login
+            </h1>
+            <p>Login and set goals</p>
+          </section>
+    
+          <section>
+            <form onSubmit={onSubmit}>
+               
+                <div className="form-group">
+                   <input
+                type="email"
+                classname="form-control"
+                id="email"
+                name="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={onChange}
+              /> 
+                </div>
+    
+                <div className="form-group">
+                   <input
+                type="password"
+                classname="form-control"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="Enter your password"
+                onChange={onChange}
+              /> 
+                </div>
+    
+              <div className="form">
+                <button type="submit" classname='btn btn-block'>Siubmit</button>
+              </div>
+            </form>
+          </section>
+        </>
+      );
+}
+export default Login
